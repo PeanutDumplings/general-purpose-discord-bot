6d8de9b5e2f0cf6d56db18bcc4f88b1ca3e83588
PeanutDumplings <nicholas.whyte@outlook.com>
2024-07-09 20:46:42 +0930

    Add ban command with full error handling and logs

---
+++ src/commands/admin/ban.ts
@@ -0,0 +1,108 @@
+import { ChannelType, ChatInputCommandInteraction, type ColorResolvable, EmbedBuilder, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import { CommandConfig } from "../../classes/CommandConfig";
+import { Command } from "../../classes/Command";
+import config from "../../config";
+
+const commandBuilder = new SlashCommandBuilder()
+    .setName("ban")
+    .setDescription("Bans a member from a guild")
+    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers);
+
+commandBuilder.addUserOption((option) => option.setName("user").setDescription("The user to ban").setRequired(true));
+commandBuilder.addStringOption((option) => option.setName("reason").setDescription("The reason for banning the user").setRequired(false));
+
+const commandConfig = new CommandConfig(commandBuilder);
+
+const command = async (interaction: ChatInputCommandInteraction) => {
+    const user = await interaction.guild?.members.fetch(interaction.options.getUser("user")?.id!);
+    const reason = interaction.options.getString("reason");
+    const member = await interaction.guild?.members.fetch(interaction.user.id);
+
+    const userEmbed = new EmbedBuilder().setColor(config.colours.failure as ColorResolvable);
+    const serverEmbed = new EmbedBuilder().setColor(config.colours.blurple as ColorResolvable);
+
+    // if (user?.user.id === member?.user.id) {
+    //     return await interaction.reply({
+    //         embeds: [
+    //             serverEmbed.setDescription(`${config.emojis.error} You cannot ban yourself`).setColor(config.colours.failure as ColorResolvable),
+    //         ],
+    //         ephemeral: true,
+    //     });
+    // }
+
+    // if (user?.roles.highest!! >= member?.roles.highest!) {
+    //     return await interaction.reply({
+    //         embeds: [
+    //             serverEmbed
+    //                 .setDescription(
+    //                     `${config.emojis.error} You cannot ban ${user?.user}\n**Reason:** Your top role is not higher than ${user?.user}'s`
+    //                 )
+    //                 .setColor(config.colours.failure as ColorResolvable),
+    //         ],
+    //         ephemeral: true,
+    //     });
+    // }
+
+    if (user?.user.id === interaction.client.user.id) {
+        return await interaction.reply({
+            embeds: [
+                serverEmbed.setDescription(`${config.emojis.error} You cannot ban this bot`).setColor(config.colours.failure as ColorResolvable),
+            ],
+            ephemeral: true,
+        });
+    }
+
+    if (!user?.bannable) {
+        return await interaction.reply({
+            embeds: [
+                serverEmbed
+                    .setDescription(`${config.emojis.error} ${user} cannot be banned by me`)
+                    .setColor(config.colours.failure as ColorResolvable),
+            ],
+            ephemeral: true,
+        });
+    }
+
+    userEmbed.setTitle("Banned");
+    userEmbed.setDescription(
+        [
+            `**Server**: \`${interaction.guild?.name}\``,
+            `**Moderator**: ${member?.user} (${member?.user.id})`,
+            `**Reason**: \`${reason ? reason : "no reason provided"}\``,
+        ].join("\n")
+    );
+    userEmbed.setTimestamp();
+
+    serverEmbed.setTitle("Banned");
+    serverEmbed.setDescription(
+        [
+            `**User**: ${user} (${user?.id})`,
+            `**Moderator**: ${member?.user} (${member?.user.id})`,
+            `**Reason**: \`${reason ? reason : "no reason provided"}\``,
+        ].join("\n")
+    );
+    serverEmbed.setColor(config.colours.blurple as ColorResolvable);
+
+    const logsChannel = await interaction.guild?.channels.fetch(config.channels.logs);
+    if (!logsChannel || logsChannel.type !== ChannelType.GuildText) {
+        return await interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setColor(config.colours.failure as ColorResolvable)
+                    .setDescription("Invalid logs channel. Please update `config.yaml` with a valid text channel id"),
+            ],
+        });
+    }
+
+    await user?.send({ embeds: [userEmbed] });
+    await user?.ban();
+    await logsChannel.send({ embeds: [serverEmbed] });
+    return await interaction.reply({
+        embeds: [
+            new EmbedBuilder()
+                .setDescription(`${config.emojis.success} ***${user}*** was banned`)
+                .setColor(config.colours.success as ColorResolvable),
+        ],
+    });
+};
+export default new Command(commandConfig, command);
