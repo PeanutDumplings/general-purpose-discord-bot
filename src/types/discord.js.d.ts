332af822507e255d719a279d6fdfd760e4ed73a1
PeanutDumplings <nicholas.whyte@outlook.com>
2024-07-09 19:12:59 +0930

    Extended djs types for events

--- src/types/discord.js.d.ts
+++ src/types/discord.js.d.ts
@@ -1,8 +1,10 @@
 import { Collection } from "discord.js";
 import type { Command } from "../classes/Command";
+import type { Event } from "../classes/Event";
 
 declare module "discord.js" {
     export interface Client {
         commands: Collection<string, Command>;
+        events: Collection<string, Event>;
     }
 }
