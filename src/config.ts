c48cd87db607d8a0281d951578570c2de8a50210
PeanutDumplings <nicholas.whyte@outlook.com>
2024-07-09 20:30:42 +0930

    Add kick command

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,31 @@ import fs from "node:fs";
 interface Config {
     discordBotToken: string;
     discordBotClientID: string;
+    channels: {
+        logs: string;
+    };
+    colours: {
+        success: string;
+        failure: string;
+        warning: string;
+        blurple: string;
+        clear: string;
+    };
+    emojis: {
+        arrow: string;
+        dash: string;
+        success: string;
+        error: string;
+        warning: string;
+        info: string;
+        staff: string;
+        developer: string;
+        banned: string;
+        bot: string;
+        join: string;
+        leave: string;
+        link: string;
+    };
 }
 
 const configPath = "./config.yaml";
@@ -11,6 +36,31 @@ const configFile = yaml.load(fs.readFileSync(configPath, "utf-8")) as Config;
 const config: Config = {
     discordBotToken: configFile.discordBotToken,
     discordBotClientID: configFile.discordBotClientID,
+    channels: {
+        logs: configFile.channels.logs,
+    },
+    colours: {
+        success: configFile.colours.success,
+        failure: configFile.colours.failure,
+        warning: configFile.colours.warning,
+        blurple: configFile.colours.blurple,
+        clear: configFile.colours.clear,
+    },
+    emojis: {
+        success: configFile.emojis.success ?? "✅",
+        error: configFile.emojis.error ?? "❌",
+        warning: configFile.emojis.warning ?? "⚠️",
+        arrow: configFile.emojis.arrow ?? "➡️",
+        dash: configFile.emojis.dash ?? "➖",
+        info: configFile.emojis.info ?? "ℹ️",
+        staff: configFile.emojis.staff ?? "👮",
+        developer: configFile.emojis.developer ?? "👨‍💻",
+        banned: configFile.emojis.banned ?? "🚫",
+        bot: configFile.emojis.bot ?? "🤖",
+        join: configFile.emojis.join ?? "📥",
+        leave: configFile.emojis.leave ?? "📤",
+        link: configFile.emojis.link ?? "🔗",
+    },
 };
 
 export default config;
